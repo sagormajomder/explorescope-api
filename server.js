b290b1fe0971f4c5ad3c0d843c79b7ab9253e31f
Sagor Majomder <sagormajomder@gmail.com>
2025-07-21 00:56:54 +0600

    Add continent route to serve continent data

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const PORT = 8000;
 const rawData = await getDataFromDatabase();
 
 const server = http.createServer((req, res) => {
+  //---> Root Route
   if (req.url === '/' && req.method === 'GET') {
     const content = JSON.stringify(rawData);
+    sendJSONResponse(res, 200, content);
+  }
+  //---> Continent Route
+  else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+    const continentName = req.url.split('/').pop();
+    // console.log(continentName);
+
+    const continentData = rawData.filter(
+      data => data.continent.toLowerCase() === continentName.toLowerCase()
+    );
+    // console.log(continentData);
+
+    const content = JSON.stringify(continentData);
     sendJSONResponse(res, 200, content);
   }
 });
